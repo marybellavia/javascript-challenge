f7eeb3e8c698802a934ecf45dd26a5a0958e719a
Mary Bellavia <sauerwein.mary@gmail.com>
2021-02-19 10:36:51 -0600

    finished first filter

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -13,3 +13,45 @@ data.forEach((ufoReport) => {
   });
 });
 
+// Select the button
+var button = d3.select("#filter-btn");
+
+// Select the form
+var form = d3.select("#form");
+
+// Create event handlers 
+button.on("click", runFilter);
+form.on("submit",runFilter);
+
+function runFilter() {
+
+  // Prevent the page from refreshing
+  d3.event.preventDefault();
+  
+  // Select the input element and get the raw HTML node
+  var inputElement = d3.select("#datetime");
+
+  // Get the value property of the input element
+  var inputValue = inputElement.property("value");
+
+  console.log(inputValue);
+  console.log(tableData);
+
+  var filteredData = tableData.filter(tableData => tableData.datetime === inputValue);
+
+  console.log(filteredData);
+
+  // Then, select the table element by class name
+  var newtbody = d3.select("tbody");
+
+  // remove any children from the list to
+  newtbody.html("");
+
+  filteredData.forEach((ufoReport) => {
+    var row = tbody.append("tr");
+    Object.entries(ufoReport).forEach(([key, value]) => {
+      var cell = row.append("td");
+      cell.text(value);
+    });
+  });
+};
